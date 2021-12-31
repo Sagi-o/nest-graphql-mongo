d83fa37aeda2050542fb631c0c81bce4329e551a
Sagi-o <sagioshri@gmail.com>
2021-12-31 14:51:30 +0200

    added validations and mongodb version fix

--- src/lesson/lesson.resolver.ts
+++ src/lesson/lesson.resolver.ts
@@ -1,15 +1,19 @@
-import { Resolver, Query } from '@nestjs/graphql';
+import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
+import { CreateLessonInput } from './inputs/create-lesson.input';
+import { LessonService } from './lesson.service';
 import { LessonType } from './lesson.type';
 
 @Resolver((type) => LessonType)
 export class LessonResolver {
+  constructor(private lessonService: LessonService) {}
+
   @Query((returns) => LessonType)
-  lesson() {
-    return {
-      id: '123',
-      name: 'English Class',
-      startDate: new Date().toISOString(),
-      endDate: new Date().toISOString(),
-    };
+  lesson(@Args('id') id: string) {
+    return this.lessonService.getLesson(id);
+  }
+
+  @Mutation((returns) => LessonType)
+  createLesson(@Args('input') createLessonInput: CreateLessonInput) {
+    return this.lessonService.createLesson(createLessonInput);
   }
 }
